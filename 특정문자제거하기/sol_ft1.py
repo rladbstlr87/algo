90e6f287e7411a243ce0a42c0ca324fbd8d2b1f6
rladbstlr87 <manylong@naver.com>
2025-02-18 10:28:50 +0900

    2번째 풀이

---
+++ 특정문자제거하기/sol_ft1.py
@@ -0,0 +1,8 @@
+def solution(my_string, letter):
+    answer = ''
+    
+    for char in my_string:
+        if char != letter:
+            answer = answer + char
+    return answer
+print(solution('abcdef', 'f'))
---
+++ 특정문자제거하기/sol_ft2.py
@@ -0,0 +1,6 @@
+def solution(my_string, letter):
+    answer = ''
+    
+
+    return answer
+print(solution('abcdef', 'f'))
